f46651c97b7bca62e7a3aec17357e1932ab209e7
Laurence Bautista <rorens05@gmail.com>
2020-12-06 19:39:21 +0800

    Add message

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,7 @@
 class Student < ApplicationRecord
+  enum gender: %w[Male Female]
+
+  def name
+    "#{first_name} #{last_name}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :messages
+  resources :announcements
   resources :students
   get 'profile', to: 'profile#index'
   get 'profile/change_password'
